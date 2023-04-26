f242a2d3a4dfaedf37c398559eee2d9bf0e10a44
Stephen <stephen.anthony.rivas@gmail.com>
2023-04-25 20:37:26 -0400

    added compression to images. will refactor to create standalone self-compressing Image component

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,41 @@
 import { useEffect, useState } from 'react'
 import './App.css'
+import imageCompression from 'browser-image-compression';
+
+const compressionOptions = {
+  maxSizeMB: 1,
+  maxWidthOrHeight: 1920,
+  useWebWorker: true
+}
 
 function App() {
   const [uploadedFiles, setUploadedFiles] = useState([]);
   const [allFiles, setAllFiles] = useState([]);
+  const [images, setImages] = useState([]);
 
   useEffect(() => {
+
     if (uploadedFiles.length) {
-      const imgArr = uploadedFiles.map((img) => URL.createObjectURL(img));
-      if (allFiles.length) {
-        setAllFiles([ ...allFiles, ...imgArr ]);
-      } else {
-        setAllFiles([...imgArr]);
+
+      const updateFiles = async() => {
+
+        const imgArr = await uploadedFiles.map(async (image, i) => {
+          const compressedFile = await imageCompression(image, compressionOptions);
+          const compressedImage = URL.createObjectURL(compressedFile);
+         
+          const Image =  <img key={i} src={compressedImage} style={{ width: '30%', height: '30%', maxHeight: '' }} />
+          setImages([...images, Image]);
+        });
+        
+        if (allFiles.length) {
+          setAllFiles([ ...allFiles, ...imgArr ]);
+        } else {
+          setAllFiles([ ...imgArr ]);
+        }
       }
+    // console.log(images)
+    updateFiles();
+
     }
   }, [uploadedFiles]);
 
@@ -20,13 +43,11 @@ function App() {
     setUploadedFiles(Object.values(e.target.files));
   }
 
-  const imgs = allFiles.map((file, i) => <img key={i} src={file} style={{ width: '30%', height: '30%', maxHeight: '' }}></img>);
-
   return (
     <>
       <input type="file" multiple onChange={handleFileSelect}></input>
       <div style={{ display: 'flex', width: '100%', height: '100%', flexWrap: 'wrap', overflow: 'none', gap: '2px' }}>
-        {imgs}
+        {images}
       </div>
      
     </>
